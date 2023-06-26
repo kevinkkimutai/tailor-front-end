428307ac74df136806bc72ff9c87d78571e0cbbe
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-26 19:53:47 +0300

    dashboard added

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -370,75 +370,84 @@
   </div>
 </div>
 
-<!-- resent transactions card -->
+<!-- recent transactions card -->
 <div class="w-full xl:w-1/3 px-3">
   <p class="text-xl font-semibold mb-3">Recent Reviews</p>
   <div class="">
-
-    <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+  
+<div class="w-full max-w-l p-1 bg-white border border-gray-200 rounded-lg shadow sm:p-2 dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full bg-gray-100 border rounded-lg flex justify-between items-center px-4 py-2 mb-0">
+        <div>
+            <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+        
+        </div>
         <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
             View all
         </a>
-   </div>
-   <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-            <li class="py-3 sm:py-4">
-  <div class="flex items-start space-x-4">
-    <div class="flex-1 min-w-0">
-      <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-        Neil Sims
-      </p>
-      <p class="text-sm flex text-gray-500 truncate dark:text-gray-400 break-words max-w-xs">
-        email@windster.com email@windster
-      </p>
     </div>
-  </div>
-</li>
-            <li class="py-3 sm:py-4">
+ 
+   <div class="flow-root">
+        <ul role="list" class="dark:divide-gray-700">
+            <li class="py-3 sm:py-4 border-b dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex items-center space-x-4">
+                  
                     <div class="flex-1 min-w-0">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Bonnie Green
+                            Neil Sims
                         </p>
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            email@windster.com
+                        <p class="text-sm text-gray-500  dark:text-gray-400">
+                            email@windster.com    email@windster.com    email@windster.com
                         </p>
                     </div>
                    
                 </div>
             </li>
-
-            <li class="py-3 sm:py-4">
+            <li class="py-3 sm:py-4 border-b dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex items-center space-x-4">
+                  
                     <div class="flex-1 min-w-0">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Lana Byrd
+                            Neil Sims
                         </p>
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            email@windster.com
+                        <p class="text-sm text-gray-500  dark:text-gray-400">
+                            email@windster.com    email@windster.com    email@windster.com
                         </p>
                     </div>
-                    
+                   
                 </div>
             </li>
-            <li class="pt-3 pb-0 sm:pt-4">
+            <li class="py-3 sm:py-4 border-b dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex items-center space-x-4">
+                  
                     <div class="flex-1 min-w-0">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Thomes Lean
+                            Neil Sims
                         </p>
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            email@windster.com
+                        <p class="text-sm text-gray-500  dark:text-gray-400">
+                            email@windster.com    email@windster.com    email@windster.com
                         </p>
                     </div>
-                    
+                   
+                </div>
+            </li>
+            <li class="py-3 sm:py-4 border-b dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex items-center space-x-4">
+                  
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Neil Sims
+                        </p>
+                        <p class="text-sm text-gray-500  dark:text-gray-400">
+                            email@windster.com    email@windster.com    email@windster.com
+                        </p>
+                    </div>
+                   
                 </div>
             </li>
         </ul>
    </div>
 </div>
+
 
   </div>
 </div>
