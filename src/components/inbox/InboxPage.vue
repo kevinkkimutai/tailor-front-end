de2cfe9deadb421d17a5fc9350169e62132ef0a8
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-27 18:31:35 +0300

    added inbox page

---
+++ src/components/inbox/InboxPage.vue
@@ -0,0 +1,142 @@
+<template lang="">
+   <div class="p-1 sm:ml-52 ">
+   <div class="p-1 border-gray-200 border-dashed rounded-sm dark:border-gray-700 mt-1">
+
+    <!-- breadcrumb -->
+         <nav class="text-sm font-semibold mb-5" aria-label="Breadcrumb">
+              <ol class="list-none p-1 inline-flex">
+                <li class="flex i text-blue-400">
+                  <a href="/Dashboard" class="text-gray-500">Dashboard</a>
+                  <svg class="fill-current w-4 h-4 mx-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/></svg>
+                </li>
+                <li class="flex items-center">
+                  <a href="/inbox" class="text-gray-600">Inbox</a>
+                </li>
+              </ol>
+            </nav> 
+            <!-- breadcrumb end -->
+
+    <div class="lg:flex justify-between items-center mb-4">
+              <p class="text-2xl font-semibold mb-2 lg:mb-0">Latest Messages . . .</p> 
+            </div>
+
+<div class="w-1/1 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-2 dark:bg-gray-800 dark:border-gray-700">
+  <div class="w-full bg-gray-200 border rounded-lg flex justify-between items-center px-4 py-2 mb-0">
+        <div>
+            <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Messages . . .</h5>
+        
+        </div>
+        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+            View all
+        </a>
+    </div>
+   <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+
+          <li class="py-3 sm:py-4">
+                <a href="#" class="flex px-10 py-3 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg">
+      <div class="flex-shrink-0">
+        <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-4.jpg" alt="Leslie image">
+        <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-green-400 border border-white rounded-full dark:border-gray-800">
+          <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </div>
+      </div>
+      <div class="w-full pl-3">
+        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+          <span class="font-semibold text-gray-900 dark:text-white">Leslie Livingston</span>
+        </p>
+          <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400 pt-2 me-5">
+             mentioned you in a comment: what do you say? mentioned you in a comment: what do you say? mentioned you in a comment: what do you say?mentioned you in a comment: what do you say?
+          </div>
+          <div class="text-xs flex flex-row text-blue-600 dark:text-blue-500">1 hour ago</div>
+          <div class="me-5 flex flex-row-reverse ...">
+            <div class="ms-3 me-5 items-center text-sm font-semibold text-green-700 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">Reply</a>
+      </div>
+      <div class="ms-3 items-center text-sm font-semibold text-purple-600 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">UnRead</a>
+      </div> 
+      <div class="ms-3 items-center text-sm font-semibold text-blue-600 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">Read</a>
+      </div>
+</div>
+      </div>
+    </a>
+            </li>
+            <li class="py-3 sm:py-4">
+                <a href="#" class="flex px-10 py-3 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg">
+      <div class="flex-shrink-0">
+        <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-4.jpg" alt="Leslie image">
+        <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-green-400 border border-white rounded-full dark:border-gray-800">
+          <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </div>
+      </div>
+      <div class="w-full pl-3">
+        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+          <span class="font-semibold text-gray-900 dark:text-white">Leslie Livingston</span>
+        </p>
+          <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400 pt-2 me-5">
+             mentioned you in a comment: what do you say? mentioned you in a comment: what do you say? mentioned you in a comment: what do you say?mentioned you in a comment: what do you say?
+          </div>
+          <div class="text-xs flex flex-row text-blue-600 dark:text-blue-500">1 hour ago</div>
+          <div class="me-5 flex flex-row-reverse ...">
+            <div class="ms-3 me-5 items-center text-sm font-semibold text-green-700 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">Reply</a>
+      </div>
+      <div class="ms-3 items-center text-sm font-semibold text-purple-600 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">UnRead</a>
+      </div> 
+      <div class="ms-3 items-center text-sm font-semibold text-blue-600 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">Read</a>
+      </div>
+</div>
+      </div>
+    </a>
+            </li>
+
+            
+            <li class="py-3 sm:py-4">
+                <a href="#" class="flex px-10 py-3 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg">
+      <div class="flex-shrink-0">
+        <img class="rounded-full w-11 h-11" src="/docs/images/people/profile-picture-4.jpg" alt="Leslie image">
+        <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-green-400 border border-white rounded-full dark:border-gray-800">
+          <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+        </div>
+      </div>
+      <div class="w-full pl-3">
+        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+          <span class="font-semibold text-gray-900 dark:text-white">Leslie Livingston</span>
+        </p>
+          <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400 pt-2 me-5">
+             mentioned you in a comment: what do you say? mentioned you in a comment: what do you say? mentioned you in a comment: what do you say?mentioned you in a comment: what do you say?
+          </div>
+          <div class="text-xs flex flex-row text-blue-600 dark:text-blue-500">1 hour ago</div>
+          <div class="me-5 flex flex-row-reverse ...">
+            <div class="ms-3 me-5 items-center text-sm font-semibold text-green-700 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">Reply</a>
+      </div>
+      <div class="ms-3 items-center text-sm font-semibold text-purple-600 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">UnRead</a>
+      </div> 
+      <div class="ms-3 items-center text-sm font-semibold text-blue-600 dark:text-white hover:underline dark:text-green-500">
+          <a href="#">Read</a>
+      </div>
+</div>
+      </div>
+    </a>
+            </li>
+        </ul>
+   </div>
+</div>
+</div>
+</div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import { createRouter,createWebHistory } from 'vue-router';
 import DashBoard from '../components/DashBoard.vue';
 import HomePage from '../components/HomePage.vue';
-
+import InboxPage from '../components/inbox/InboxPage.vue'
 
 const routes = [
     { path: '/', component: HomePage},
     { path: '/dashboard', component: DashBoard},
+    { path: '/inbox', component: InboxPage},
   
 ];
 
