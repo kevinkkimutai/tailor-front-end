ec3a4ff9691a1eecb26621c88e13397d70ae7abe
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-28 17:29:38 +0300

    men shirts uploaded

--- src/components/products/MenProduct.vue
+++ src/components/products/MenProduct.vue
@@ -11,20 +11,20 @@
                     <svg class="fill-current w-4 h-4 mx-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/></svg>
                   </li>
                   <li class="flex items-center">
-                    <a href="/products" class="text-gray-600">Products</a>
+                    <a href="/products" class="text-gray-600">Men Products</a>
                   </li>
                 </ol>
               </nav> 
               <!-- breadcrumb end -->
    
       <div class="lg:flex justify-between items-center mb-3 ms-2">
-                <p class="text-2xl font-semibold mb-2 lg:mb-0">List of Ladies Designs . . .</p> 
+                <p class="text-2xl font-semibold mb-2 lg:mb-0">List of Men Designs . . .</p> 
               </div>
    
    <!-- product section -->
    
    <div class="lg:flex justify-between items-center ms-10">
-                <p class="text-2xl font-semibold mb-2 lg:mb-0"><span class="text-gray-400">1.</span> DRESSES</p> 
+                <p class="text-2xl font-semibold mb-2 lg:mb-0"><span class="text-gray-400">1.</span> TRAUSERS</p> 
               </div>
     <div class="flex flex-wrap -mx-3 mb-4">
      
@@ -32,7 +32,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full pt-1 rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/dress1.webp" alt="">
+       <img class="object-cover w-full pt-1 rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/men1.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -44,7 +44,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/dress22.png" alt="">
+       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/men2.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -56,7 +56,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/dress331.jpg" alt="">
+       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/men3.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -68,7 +68,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/dress22.png" alt="">
+       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg pt-1" src="../../assets/men4.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -81,7 +81,7 @@
    
    <!-- start trausers -->
    <div class="lg:flex justify-between items-center ms-10">
-                <p class="text-2xl font-semibold mb-2 lg:mb-0"><span class="text-gray-400">2.</span> TRAUSERS</p> 
+                <p class="text-2xl font-semibold mb-2 lg:mb-0"><span class="text-gray-400">2.</span> SHIRTS</p> 
               </div>
     <div class="flex flex-wrap -mx-3 mb-4">
      
@@ -89,7 +89,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full pt-1 rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/trauser1.png" alt="">
+       <img class="object-cover w-full pt-1 rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/shirt1.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -101,7 +101,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/trauser2.jpeg" alt="">
+       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/shirt2.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -113,7 +113,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/trauser3.png" alt="">
+       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/shirt3.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -125,7 +125,7 @@
    <div class="w-full xl:w-1/2 px-3">
      <p class="text-xl font-semibold mb-4"></p>
      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/trauser44.png" alt="">
+       <img class="object-cover w-full rounded-t-lg h-100 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="../../assets/shirt4.png" alt="">
        <div class="flex flex-col justify-between p-4 leading-normal">
            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
