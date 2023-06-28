8dc791275fb750a306bb700f0a29fc374dab2e6f
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-28 19:38:48 +0300

    loginpage added and styled

--- src/components/auth/LoginPage.vue
+++ src/components/auth/LoginPage.vue
@@ -2,9 +2,9 @@
 <!-- component -->
 <div class="p-1 sm:ml-52 bg-white">
   <div class="p-1 border-gray-200 border-dashed rounded-sm dark:border-gray-700 mt-1">
-    <section class=" flex items-stretch text-white p-10 rounded-lg">
-        <div class="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center logimage" style="background-image: url(../../assets/logo1.png)">
-            <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
+    <section class=" flex items-stretch text-white p-9 rounded-lg">
+        <div class="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center logimage" stye="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
+            <div class="absolute bg-black opacity-70 inset-0 z-0"></div>
             <div class="w-full px-24 z-10">
                 <h1 class="text-5xl font-bold text-left tracking-wide">Keep it special</h1>
                 <p class="text-3xl my-4">Capture your personal memory in unique way, anywhere.</p>
@@ -22,7 +22,7 @@
             </div>
         </div>
         <div class="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0" style="background-color: #161616;">
-            <div class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center" style="background-image: url(../../assets/logo1.png);">
+            <div class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center logimage" stye="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
                 <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
             </div>
             <div class="w-full py-6 z-20">
@@ -75,8 +75,8 @@ export default {
     
 }
 </script>
-<style lang="">
+<style scoped>
 .logimage {
-    background-image: url(../../assets/logo1.png);
+    background-image: url(../../assets/dress331.jpg);
 }
 </style>
