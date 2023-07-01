f44b0713aa2f50a6b05dc27c8c84e1daf94f12ab
kevinkkimutai <kiruikevinkim@gmail.com>
2023-07-01 20:13:58 +0300

    added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <NavBar /> -->
+     <NavBar /> 
 
   <div class="containe text-white">
     <RouterView />
@@ -7,10 +7,10 @@
 </template>
 
 <script>
-// import NavBar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue'
 export default {
   components: {
-            
+          NavBar,  
            
       
   },
