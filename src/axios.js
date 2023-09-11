bd64b5ad09037d206c23c5fd5f9c4981aaf66704
kevinkkimutai <kiruikevinkim@gmail.com>
2023-09-11 14:47:42 +0300

    commented

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
-
+// default backemd api url
 axios.defaults.baseURL = 'http://localhost:8000/api/';
+
+// default hrader auth  token retriever
 axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token');
