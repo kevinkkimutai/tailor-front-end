ece97e8c92c4760bd316ee8c12e9fe6e017ffb6e
kevinkkimutai <kiruikevinkim@gmail.com>
2023-09-14 21:16:48 +0300

    comments

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -153,6 +153,7 @@ export default {
   }
 }
 </script>
+// colateral damage 
 
 <section class="flex items-stretch text-white p-9  loginsect" style="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
   <div class="lg:flex w-1/2 hidden bg-no-repeat bg-cover h-full relate items-center logimage" style="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
